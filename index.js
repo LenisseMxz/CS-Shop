b644825bbba4d29848815aff6aab1097faa84c92
Montserrat <llopez95@ucol.mx>
2026-05-17 12:50:59 -0600

    Update 4 (Pre Working slider and minor adjustments

--- index.js
+++ index.js
@@ -5,36 +5,48 @@ const btnRightSlide = document.getElementById("btn-right-slide");
 const myImages = ["./media/Front-page1.jpeg", "./media/Front-page2.png", "./media/Front-page3.png"]
 let image = 0;
 
-btnLeftSlide.addEventListener("click", () => {
-
-    if (image > -2) {
-        image--;
-        if (image > 0) {
-            imgFrontPage.setAttribute("src", myImages[image]);
-        } else {
-            image = -1 * (image)
-            imgFrontPage.setAttribute("src", myImages[image]);
-            image = -1 * (image)
-        }
-    } else {
+setInterval(() => {
+    imgFrontPage.animate([
+        {opacity: 0},
+        {opacity: 1}
+    ], {
+        duration: 1000,
+        easing: "ease-in-out"
+    })
+    
+    if (image >= 2) {
         image = 0;
         imgFrontPage.setAttribute("src", myImages[image]);
+    } else if (image <= -2) {
+        image++;
+        image = -1 * image;
+        imgFrontPage.setAttribute("src", myImages[image]);
+        image = -1 * image;
+    } else {
+        image++;
+        imgFrontPage.setAttribute("src", myImages[image]);
+    }
+
+}, 3000);
+
+btnLeftSlide.addEventListener("click", () => {
+    if (image <= -2) {
+        image = 0;
+        imgFrontPage.setAttribute("src", myImages[image]);
+    } else {
+        image--;
+        image = -1 * image;
+        imgFrontPage.setAttribute("src", myImages[image]);
+        image = -1 * image;
     }
 })
 
 btnRightSlide.addEventListener("click", () => {
-
-    if (image < 2) {
-        image++;
-        if (image < 0) {
-            image = -1 * (image)
-            imgFrontPage.setAttribute("src", myImages[image]);
-            image = -1 * (image)
-        } else {
-          imgFrontPage.setAttribute("src", myImages[image]);
-        }
-    } else {
+    if (image >= 2) {
         image = 0;
+        imgFrontPage.setAttribute("src", myImages[image]);
+    } else {
+        image++;
         imgFrontPage.setAttribute("src", myImages[image]);
     }
 })
