3474de0c53ac8d6cb855e1082820d401b8de0b60
Montserrat <llopez95@ucol.mx>
2026-05-20 11:14:49 -0600

    Minor adjustments + slider discarded

--- index.js
+++ index.js
@@ -1,73 +1,53 @@
-let divMessage = document.getElementById("div-message");
-let divMain = document.getElementById("div-main");
+let productMessage = document.getElementById("product-message");
+let main = document.getElementById("main");
 
 const btnProducts = document.getElementById("btn-products");
 const btnHome = document.getElementById("btn-home");
+const btnShoppingCart = document.getElementById("btn-shoppingcart");
 
-const imgFrontPage = document.getElementById("img-front-page");
-const btnLeftSlide = document.getElementById("btn-left-slide");
-const btnRightSlide = document.getElementById("btn-right-slide");
+function add(id, price) {
+    let productId = id;
+    let productQuantity = document.getElementById("product-quantity").value;
+    let productPrice = price;
 
-const myImages = ["./media/Front-page1.jpeg", "./media/Front-page2.png", "./media/Front-page3.png"]
-let image = 0;
+    // fetch POST
 
-setInterval(() => {
-    imgFrontPage.animate([
+    productMessage.style.visibility = "visible";
+    productMessage.innerHTML = `<p style="color: #362317";>${"The product has been added to the shopping cart"}</p>`;
+
+    productMessage.animate([
         {opacity: 0},
         {opacity: 1}
     ], {
-        duration: 1000,
-        easing: "ease-in-out"
-    })
+        duration: 500,
+        easing: "ease-in"
+    });
     
-    if (image >= 2) {
-        image = 0;
-        imgFrontPage.setAttribute("src", myImages[image]);
-    } else if (image <= -2) {
-        image++;
-        image = -1 * image;
-        imgFrontPage.setAttribute("src", myImages[image]);
-        image = -1 * image;
-    } else {
-        image++;
-        imgFrontPage.setAttribute("src", myImages[image]);
-    }
+    setTimeout(() => {
+        productMessage.animate([
+            {opacity: 1},
+            {opacity: 0}
+        ], {
+            duration: 500,
+            easing: "ease-out"
+        });
+    }, 3000);
 
-}, 3000);
-
-btnLeftSlide.addEventListener("click", () => {
-    if (image <= -2) {
-        image = 0;
-        imgFrontPage.setAttribute("src", myImages[image]);
-    } else {
-        image--;
-        image = -1 * image;
-        imgFrontPage.setAttribute("src", myImages[image]);
-        image = -1 * image;
-    }
-})
-
-btnRightSlide.addEventListener("click", () => {
-    if (image >= 2) {
-        image = 0;
-        imgFrontPage.setAttribute("src", myImages[image]);
-    } else {
-        image++;
-        imgFrontPage.setAttribute("src", myImages[image]);
-    }
-})
+    setTimeout(() => {
+        productMessage.innerHTML = "";
+        productMessage.style.visibility = "hidden";
+    }, 3500)
+}
 
 btnHome.addEventListener("click", () => {
-    divMain.removeAttribute("class");
-    divMain.innerHTML = "";
-    divMain.innerHTML = `
+    main.removeAttribute("class");
+    main.innerHTML = "";
+    main.innerHTML = `
                         <h3>Welcome to CS Shop</h3>
                         <p>Wanna be pretty? in CS Shop you can find a wide variety of high-quality beauty products.</p>
-                        <div class="div-img">
+                        <div class="div-home-img">
                             <div class="row">
-                                <button id="btn-left-slide" class="btn-slide"><</button>
-                                <img id="img-front-page" src="./media/Front-page1.jpeg" alt="Front page">
-                                <button id="btn-right-slide" class="btn-slide">></button>
+                                <img id="img-front-page" src="./media/Front-page.jpeg" alt="Front page">
                             </div>
                         </div>
                         `;
@@ -80,50 +60,35 @@ btnProducts.addEventListener("click", () => {
     //     products.forEach(product => {})
     // })
 
-    divMain.setAttribute("class", "row-wrap");
-    divMain.innerHTML = "";
-    divMain.innerHTML += `
-                        <div id="div-product${1}" class="div-products" style="width: 30%">
+    main.setAttribute("class", "row-wrap");
+    main.innerHTML = "";
+    main.innerHTML += `
+                        <div id="product${1}" class="div-products">
                             <h4>${"My Foundation"}</h4>
-                            <img src=${"./media/Foundation.jpeg"} alt=${"My Foundation"} style="width: 50%">
+                            <img class="img-products" src=${"./media/Foundation.jpeg"} alt=${"My Foundation"}>
                             <p>${"$20"}</p>
                             <p>${"Expensive Foundation"}</p>
-                            <input type="number" id="input-quantity" class="input-products" name="quantity" min="1" max="5" value=""></input>   
-                            <button type="button" id="btn-add" class="btn-others"onclick="add(1, 20)">Add</button>
+                            <input type="number" id="product-quantity" class="input-products" name="quantity" min="1" max="5"></input>   
+                            <button type="button" onclick="add(product${1}, ${20})" class="btn-products">Add</button>
                         </div>
                         `;
 })
 
-function add(id, price) {
-    let productId = id;
-    let productQuantity = document.getElementById("input-quantity").value;
-    let productPrice = price;
-
-    // fetch POST
-
-    divMessage.style.visibility = "visible";
-    divMessage.innerHTML = `<p style="color: #362317";>${"The product has been added to the shopping cart"}</p>`;
-
-    divMessage.animate([
-        {opacity: 0},
-        {opacity: 1}
-    ], {
-        duration: 500,
-        easing: "ease-in"
-    });
-    
-    setTimeout(() => {
-        divMessage.animate([
-            {opacity: 1},
-            {opacity: 0}
-        ], {
-            duration: 500,
-            easing: "ease-out"
-        });
-    }, 3000);
-
-    setTimeout(() => {
-        divMessage.innerHTML = "";
-        divMessage.style.visibility = "hidden";
-    }, 3500)
-}
+btnShoppingCart.addEventListener("click", () => {
+    // fetch GET
+    main.removeAttribute("class");
+    main.innerHTML = "";
+    main.innerHTML += `
+                        <div class="row">
+                            <img id="sc-image" src=${"./media/Foundation.jpeg"} alt=${"My Foundation"}>
+                            <div>
+                                <h5>${"My Foundation"}</h5>
+                                <p class="text-sc-content">${"Expensive Foundation"}</p>
+                            </div>
+                        </div>
+                        <div id="div-sc-secondary" style="background-color: #f0e3db">
+                            <h3>Total</h3>
+                            <p>${"$20"}</p>
+                        </div>
+                        `;
+})
