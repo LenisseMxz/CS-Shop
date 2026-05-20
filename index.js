8f3976299f25cccbfd7122abb006410bcaf12383
Montserrat <llopez95@ucol.mx>
2026-05-20 12:21:57 -0600

    Actualizacion pequeña visual + ajustes

--- index.js
+++ index.js
@@ -45,10 +45,8 @@ btnHome.addEventListener("click", () => {
     main.innerHTML = `
                         <h3>Welcome to CS Shop</h3>
                         <p>Wanna be pretty? in CS Shop you can find a wide variety of high-quality beauty products.</p>
-                        <div class="div-home-img">
-                            <div class="row">
-                                <img id="img-front-page" src="./media/Front-page.jpeg" alt="Front page">
-                            </div>
+                        <div id="home-frame">
+                            <img id="home-img" src="./media/Front-page.jpeg" alt="Front page">
                         </div>
                         `;
 }) 
@@ -63,32 +61,33 @@ btnProducts.addEventListener("click", () => {
     main.setAttribute("class", "row-wrap");
     main.innerHTML = "";
     main.innerHTML += `
-                        <div id="product${1}" class="div-products">
-                            <h4>${"My Foundation"}</h4>
-                            <img class="img-products" src=${"./media/Foundation.jpeg"} alt=${"My Foundation"}>
-                            <p>${"$20"}</p>
-                            <p>${"Expensive Foundation"}</p>
-                            <input type="number" id="product-quantity" class="input-products" name="quantity" min="1" max="5"></input>   
-                            <button type="button" onclick="add(product${1}, ${20})" class="btn-products">Add</button>
-                        </div>
-                        `;
+                    <div class="div-products">
+                        <h4>${"My Foundation"}</h4>
+                        <img class="img-products" src=${"./media/Foundation.jpeg"} alt=${"My Foundation"}>
+                        <p>${"$20"}</p>
+                        <p>${"Expensive Foundation"}</p>
+                        <input type="number" id="product-quantity" class="input-products" name="quantity" min="1" max="5"></input>   
+                        <button type="button" onclick="add(product${1}, ${20})" class="btn-products">Add</button>
+                    </div>
+                    `;
 })
 
 btnShoppingCart.addEventListener("click", () => {
     // fetch GET
     main.removeAttribute("class");
     main.innerHTML = "";
-    main.innerHTML += `
-                        <div class="row">
-                            <img id="sc-image" src=${"./media/Foundation.jpeg"} alt=${"My Foundation"}>
-                            <div>
-                                <h5>${"My Foundation"}</h5>
-                                <p class="text-sc-content">${"Expensive Foundation"}</p>
+    main.innerHTML += `<div id="sc-main-frame">
+                            <div class="row">
+                                <img id="sc-image" src=${"./media/Foundation.jpeg"} alt=${"My Foundation"}>
+                                <div>
+                                    <h5>${"My Foundation"}</h5>
+                                    <p class="text-sc-content">${"Expensive Foundation"}</p>
+                                </div>
                             </div>
-                        </div>
-                        <div id="div-sc-secondary" style="background-color: #f0e3db">
-                            <h3>Total</h3>
-                            <p>${"$20"}</p>
+                            <div id="sc-secondary-frame">
+                                <h3>Total</h3>
+                                <p>${"$20"}</p>
+                            </div>
                         </div>
                         `;
 })
